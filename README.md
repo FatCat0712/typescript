ea1ae673420b47d13e389c1cc2436650eb37cea2
Fatcat0712 <maisonhai456@gmail.com>
2026-03-21 17:34:50 +0700

    number and string primitive type in typescript

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+### Bài 1: Cài đặt môi trường
+
 - typescript: compilier chính
 - ts-node: chạy file .ts trực tiếp không cần buil ra .js
 - @types/node: giúp TypeScript hiểu các hàm Node js (như console, process).
+
+### Bài 2: Kiểu dữ liệu cơ bản trong TypeScript
---
+++ src/b02/primitive.ts
@@ -0,0 +1,27 @@
+// string
+
+// const courseTitle: string = "Typescript";
+// const author = "Dung";
+
+// // template string
+// const banner: string = `Course ${courseTitle} by ${author}`;
+// console.log(banner);
+
+// ham
+// function greet(name: string): string {
+//   return `Hello ${name}`;
+// }
+// console.log(greet("Dung"));
+
+// number
+// const version = 1.0;
+// const price = 9_9_9;
+// const ratio: number = 0.75;
+
+// console.log(price);
+
+// function add(a: number, b: number): number {
+//   return a + b;
+// }
+
+// console.log(add("1", 2));
