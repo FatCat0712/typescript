fe3dbc683604900ededb7d6f93b2a30c4006d9ac
Fatcat0712 <maisonhai456@gmail.com>
2026-03-24 23:58:58 +0700

    generice interface in ts

--- src/b07/index.ts
+++ src/b07/index.ts
@@ -13,26 +13,44 @@
 // console.log(getFirst(["a", "b", "c"]));
 // console.log(getFirst([]));
 
-function printLength<T extends { length: number }>(item: T): number {
-  return item.length;
+// function printLength<T extends { length: number }>(item: T): number {
+//   return item.length;
+// }
+
+// console.log(printLength("hello"));
+// console.log(printLength([1, 2, 3, 4]));
+// // console.log(printLength(43));
+
+// const str = "hello";
+// console.log(str.length);
+// // Tương đương với
+// // 1. Javascript tạo một String object: new String("hello")
+// // 2. Gọi phương thức length trên String object đó: new String("hello").length
+// // 3. Trả về 5 và huỷ string object tạm thời
+
+// const strPrimitive = "hello"; // "Primitive"
+// const strObject = new String("hello"); // "object"
+
+// console.log(typeof strPrimitive); // "string"
+// console.log(typeof strObject); // "object"
+
+// console.log(strPrimitive.length); // 5 (autoboxing)
+// console.log(strObject.length); // 5 (truy cập trực tiếp trên object)
+
+interface StringBox {
+  content: string;
 }
 
-console.log(printLength("hello"));
-console.log(printLength([1, 2, 3, 4]));
-// console.log(printLength(43));
+interface NumberBox {
+  content: number;
+}
 
-const str = "hello";
-console.log(str.length);
-// Tương đương với
-// 1. Javascript tạo một String object: new String("hello")
-// 2. Gọi phương thức length trên String object đó: new String("hello").length
-// 3. Trả về 5 và huỷ string object tạm thời
+interface Box<T> {
+  value: T;
+}
 
-const strPrimitive = "hello"; // "Primitive"
-const strObject = new String("hello"); // "object"
+let numBox: Box<number> = { value: 42 };
+let strBox: Box<string> = { value: "Hello" };
 
-console.log(typeof strPrimitive); // "string"
-console.log(typeof strObject); // "object"
-
-console.log(strPrimitive.length); // 5 (autoboxing)
-console.log(strObject.length); // 5 (truy cập trực tiếp trên object)
+console.log(numBox.value);
+console.log(strBox.value);
